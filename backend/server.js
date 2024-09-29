ab22dc4310b40411a97b068c81e1facded74bec1
zeyad <zeiad_agamy@icloud.com>
2024-09-29 16:08:59 +0300

    ready

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,29 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import { connectDB } from './config/db.js';
-import activityRoutes from './routes/activity.route.js'; 
-import itineraryRoutes from './routes/itinerary.route.js';
-import historicalplacesRoutes from './routes/historicalplaces.route.js';
-
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+import activityRoutes from "./routes/activity.route.js";
+import itineraryRoutes from "./routes/itinerary.route.js";
+import historicalplacesRoutes from "./routes/historicalplaces.route.js";
 
 dotenv.config();
 
 const app = express();
 
-
 app.use(express.json());
 
-connectDB().then(() => {
-    app.listen(process.env.PORT || 5000, () => { 
-        console.log(`Server started at http://localhost:${process.env.PORT || 5000}`);
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 5001, () => {
+      console.log(
+        `Server started at http://localhost:${process.env.PORT || 5000}`
+      );
     });
-}).catch(error => {
-    console.error('Database connection failed. Server not started.', error);
+  })
+  .catch((error) => {
+    console.error("Database connection failed. Server not started.", error);
     process.exit(1);
-});
+  });
 
-
-app.use('/api/activities', activityRoutes); 
-app.use('/api/itineraries', itineraryRoutes);
-app.use('/api/historicalplace',historicalplacesRoutes);
-
+app.use("/api/activities", activityRoutes);
+app.use("/api/itineraries", itineraryRoutes);
+app.use("/api/historicalplace", historicalplacesRoutes);
