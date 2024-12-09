8f08e99d8fb96c3191ceeb5178b72ead3f1f768d
anastamerr <anastamerr@outlook.com>
2024-12-09 05:11:48 +0200

    fixed booking rating error

--- backend/models/booking.model.js
+++ backend/models/booking.model.js
@@ -32,7 +32,7 @@ const bookingSchema = new mongoose.Schema(
     },
     status: {
       type: String,
-      enum: ["pending", "confirmed", "completed", "cancelled"],
+      enum: ["pending", "confirmed", "completed", "cancelled","attended"],
       default: "pending",
     },
     bookingDate: {
